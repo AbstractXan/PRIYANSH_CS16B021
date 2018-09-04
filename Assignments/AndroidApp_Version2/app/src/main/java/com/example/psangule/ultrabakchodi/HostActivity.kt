098a74372d3b7b37ec80a65ebd06b8eceb8d0d2d
Priyansh Sangule <priyansh0506@gmail.com>
2018-09-04 06:06:44 +0100

    Added assignments

---
+++ Assignments/AndroidApp_Version2/app/src/main/java/com/example/psangule/ultrabakchodi/HostActivity.kt
@@ -0,0 +1,12 @@
+package com.example.psangule.ultrabakchodi
+
+import android.app.Activity
+import android.os.Bundle
+
+class HostActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_host)
+    }
+}
